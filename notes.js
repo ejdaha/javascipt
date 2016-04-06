4d6dad0ea2a52d9992e005d5f26b40e1c8bb79fb
Ejdaha <rufatba11@gmail.com>
2016-04-05 21:42:16 -0400

    Update notes.js

--- notes.js
+++ notes.js
@@ -143,4 +143,43 @@ newParagraph.innerHTML="Your Text";
 document.getElementById("Id name").appendChild(newHeading);
 document.getElementById("Id name").appendChild(newParagraph);
 
+///Switch Statement:
 
+function myTest(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val){
+      
+    case 1:
+    return "alpha";   
+    break;
+         case 2:
+    return "beta";   
+    break;
+         case 3:
+    return "gamma";   
+    break;
+         case 4:
+    return "delta";   
+    break;
+      
+      
+     
+  }
+   ////Returning Boolean Values from Functions
+  
+  
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+myTest(1);
+
+function isLess(a, b) {
+  // Fix this code
+  return a<b; 
+}
+
+// Change these values to test
+isLess(10, 15);
